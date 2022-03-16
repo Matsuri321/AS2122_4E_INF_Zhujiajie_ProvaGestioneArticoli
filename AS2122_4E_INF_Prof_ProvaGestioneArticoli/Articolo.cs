f0715a683c6d6b70785e69b78491808a1d560848
Matsurii1 <Zhujiajie233@gmail.com>
2022-03-16 10:22:24 +0100

    TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -12,17 +12,31 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
         int codice;
         string descrizione;
         string unitaMisura;
-        string prezzo; 
+        string prezzo;
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
-
+        public string Descrizione
+        {
+                 get { return descrizione; }
+        }
+        public string UnitaMisura
+        {
+                 get { return unitaMisura; }
+        }
+        public double Prezzo
+        {
+                get { return prezzo; }
+        }
 
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
             // ...
-
+            this.codice = codice;
+            this.descrizione = descrizione;
+            this.unitaMisura = unitaMisura;
+            this.prezzo = prezzo;
         }
 
         public string Visualizzati()
